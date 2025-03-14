94bc782f23d0ed4f0bd161af2c51ed7398361a88
SrHatcher <erickomaldonado11@gmail.com>
2025-03-14 02:47:37 +0000

    Culminacion del tiempo de examen

--- app/home/index.tsx
+++ app/home/index.tsx
@@ -1,14 +1,26 @@
 import { View, Text, TextInput, ScrollView, Button, Pressable } from 'react-native'
 import React, { useState } from 'react'
 import {Picker} from '@react-native-picker/picker';
+import DatePicker from 'react-native-date-picker'
 
-const HomeScreen = () => {
+const HomeScreen = () => { 
+    //HORA: 20:42
+    //hasta ahora recordé que tenia que hacer un hook para manejar todos estos estados y la validacion del formulario T.T
     const [codigo, setCodigo] = useState("")
     const [producto, setProducto] = useState("")
     const [selectedCategory, setSelectedCategory] = useState();
+    const [cantidad, setCantidad] = useState(0)
+    const [precio, setPrecio] = useState(0)
+    const [comentario, setComentario] = useState("")
 
+    const [date, setDate] = useState(new Date())
+    const [open, setOpen] = useState(false)
+
+    //No pude terminar la validacion de los campos, perdon u.u
     const comprobarFormulario= ()=>{
-        
+        if(codigo.length >= 11){
+
+        }
     }
 
   return (
@@ -42,17 +54,30 @@ const HomeScreen = () => {
 
                 <View className='mt-8'>
                     <Text>Cantidad</Text>
-                    <TextInput className='shadow-[0px_0px_2px_rgba(0,0,0,0.5)] p-4' ></TextInput>
+                    <TextInput className='shadow-[0px_0px_2px_rgba(0,0,0,0.5)] p-4' onChangeText={amount => setCantidad(Number(amount))}></TextInput>
                 </View>
 
                 <View className='mt-8'>
                     <Text>Precio unitario</Text>
-                    <TextInput className='shadow-[0px_0px_2px_rgba(0,0,0,0.5)] p-4' ></TextInput>
+                    <TextInput className='shadow-[0px_0px_2px_rgba(0,0,0,0.5)] p-4' onChangeText={price => setPrecio(Number(price))}></TextInput>
                 </View>
-
+                
+                {/*------ por alguna razon no funciona el datePicker :c ------ */}
                 <View className='mt-8'>
                     <Text>Fecha de ingreso</Text>
-                    
+                    <Button title="Open" onPress={() => setOpen(true)} />
+                    <DatePicker
+                        modal
+                        open={open}
+                        date={date}
+                        onConfirm={(date) => {
+                        setOpen(false)
+                        setDate(date)
+                        }}
+                        onCancel={() => {
+                        setOpen(false)
+                        }}
+                    />
                 </View>
 
                 <View className='mt-8'>
@@ -63,6 +88,7 @@ const HomeScreen = () => {
                         multiline
                         numberOfLines={5}
                         maxLength={40}
+                        onChangeText={text => setComentario(text)}
                         />
                 </View>
                 <View className='mt-8'>
